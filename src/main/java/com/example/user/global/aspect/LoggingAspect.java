a50ec7161d52e864b1428a2ccd316f9c394235f3
comodo <comodo@comodos-MacBook.local>
2025-07-12 20:03:39 +0900

    feat :: log 구분할 수 있도록 식별자 추가 #BACKEND-59

--- src/main/java/com/example/user/global/aspect/LoggingAspect.java
+++ src/main/java/com/example/user/global/aspect/LoggingAspect.java
@@ -7,19 +7,22 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 @Aspect
 @Component
 @Slf4j
 public class LoggingAspect {
+    private static AtomicLong counter = new AtomicLong();
 
     @Pointcut("execution(* com.example.user.domain.service..*.*(..))")
     public void userServiceMethodLogging() {}
 
     @Around("userServiceMethodLogging()")
     public Object serviceMethodLogging(ProceedingJoinPoint joinPoint) throws Throwable {
-        log.trace("Entering {}", joinPoint.getSignature().getName());
+        log.trace("Entering {} by {}", joinPoint.getSignature().getName(), counter.get());
         Object result = joinPoint.proceed();
-        log.trace("Exiting {}", joinPoint.getSignature().getName());
+        log.trace("Exiting {} by {}", joinPoint.getSignature().getName(), counter.getAndIncrement());
         return result;
     }
 }
