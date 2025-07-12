8e139a3b63d97ba8bdaa985311e048ff7a4be67c
comodo <comodo@comodos-MacBook.local>
2025-07-12 16:27:51 +0900

    fix :: response dto mapper -> http util 책임 이전 #BACKEND-59

--- src/main/java/com/example/user/domain/controller/RetrieveUserController.java
+++ src/main/java/com/example/user/domain/controller/RetrieveUserController.java
@@ -1,9 +1,9 @@
 package com.example.user.domain.controller;
 
-import com.example.user.global.mapper.ResponseDtoMapper;
 import com.example.user.domain.dto.request.UserRetrieveRequest;
-import com.example.user.global.mapper.dto.ResponseDto;
+import com.example.user.global.dto.ResponseDto;
 import com.example.user.domain.service.UserService;
+import com.example.user.global.util.HttpUtil;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -15,13 +15,12 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/users/retrieve")
 public class RetrieveUserController {
   private final UserService userService;
-  private final ResponseDtoMapper responseDtoMapper;
 
   @PatchMapping("/password")
   @ResponseStatus(HttpStatus.NO_CONTENT)
   public ResponseEntity<ResponseDto<Void>> changePassword(@RequestBody @Valid UserRetrieveRequest request) {
     userService.changePassword(request.email(), request.password());
-    ResponseDto<Void> responseDto = responseDtoMapper.toResponseDto("change password", null);
+    ResponseDto<Void> responseDto = HttpUtil.success("change password");
     return ResponseEntity
             .status(HttpStatus.ACCEPTED)
             .body(responseDto);
@@ -30,7 +29,7 @@ public class RetrieveUserController {
   @PostMapping("/userId")
   public ResponseEntity<ResponseDto<String>> retrieveUserId(@RequestBody @Valid UserRetrieveRequest request) {
     String userId = userService.retrieveUserId(request.email(), request.password());
-    ResponseDto<String> responseDto = responseDtoMapper.toResponseDto("retrieve userId", userId);
+    ResponseDto<String> responseDto = HttpUtil.success("retrieve userId", userId);
     return ResponseEntity.ok(responseDto);
   }
 
