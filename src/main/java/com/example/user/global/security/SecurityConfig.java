4c9e721973f4923cbc0f113b7495307b26455744
yuuki08noah <yuuki08noah@gmail.com>
2025-11-11 00:33:20 +0900

    fix :: Config Conflict

---
+++ src/main/java/com/example/user/global/security/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.example.user.global.security;
+
+import jakarta.servlet.FilterChain;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
+  @Bean
+  public SecurityFilterChain servletFilterChain(HttpSecurity http) throws Exception {
+    http
+            .cors(AbstractHttpConfigurer::disable)
+            .csrf(AbstractHttpConfigurer::disable);
+
+    http
+            .authorizeHttpRequests(auth -> auth
+                            .anyRequest().permitAll()
+                    );
+
+    return http.build();
+  }
+}
