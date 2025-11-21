1cc257b3c2e6a556bcab71d558ce0e6a5d9da2b6
yuuki08noah <yuuki08noah@gmail.com>
2025-11-21 14:11:40 +0900

    feat :: Filter검색추가 [PW-382]

--- src/main/java/com/example/user/domain/repository/UserRepository.java
+++ src/main/java/com/example/user/domain/repository/UserRepository.java
@@ -2,6 +2,7 @@ package com.example.user.domain.repository;
 
 import com.example.user.domain.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -10,7 +11,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, String> {
+public interface UserRepository extends JpaRepository<User, String>, JpaSpecificationExecutor<User> {
   boolean existsUserByEmail(String email);
 
   boolean existsByUserId(String userId);
