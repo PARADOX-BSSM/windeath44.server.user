749b264eb94d2a325c124c638387f71c4f50c8e6
pdh0128 <pdh0128a@gmail.com>
2025-12-15 17:10:42 +0900

    feat :: 조회 시 xp, level도 반환

--- src/main/java/com/example/user/domain/dto/response/UserResponse.java
+++ src/main/java/com/example/user/domain/dto/response/UserResponse.java
@@ -1,10 +1,14 @@
 package com.example.user.domain.dto.response;
 
+import com.example.user.domain.model.type.Level;
+
 public record UserResponse (
         String userId,
         String name,
         Long remainToken,
         String profile,
-        String role
+        String role,
+        Long xp,
+        Level level
 ) {
 }
