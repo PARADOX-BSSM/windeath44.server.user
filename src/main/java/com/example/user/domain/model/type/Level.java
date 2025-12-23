d16be017159c0c7d147960f31823ae111129c534
pdh0128 <pdh0128a@gmail.com>
2025-12-23 19:41:05 +0900

    feat :: next level 계산 메서드 추가

--- src/main/java/com/example/user/domain/model/type/Level.java
+++ src/main/java/com/example/user/domain/model/type/Level.java
@@ -1,26 +1,57 @@
 package com.example.user.domain.model.type;
 
+import lombok.AllArgsConstructor;
+
 public enum Level {
     // 조문객, 환자, 송장, 유령, 악귀
-    MOURNER,
-    PATIENT,
-    CORPSE,
-    GHOST,
-    DEMON;
+    MOURNER(0L),
+    PATIENT(10_000L),
+    CORPSE(35_000L),
+    GHOST(90_000L),
+    DEMON(200_000L);
+
+    private final long minXp;
+
+    Level(long minXp) {
+        this.minXp = minXp;
+    }
 
     public static Level fromXp(long xp) {
-        if (xp >= 200_000) {
+        if (xp >= DEMON.minXp) {
             return DEMON;
         }
-        if (xp >= 90_000) {
+        if (xp >= GHOST.minXp) {
             return GHOST;
         }
-        if (xp >= 35_000) {
+        if (xp >= CORPSE.minXp) {
             return CORPSE;
         }
-        if (xp >= 10_000) {
+        if (xp >= PATIENT.minXp) {
             return PATIENT;
         }
         return MOURNER;
+    }
+
+
+    public static long calculateXpToNextLevel(long currentXp) {
+        long safeXp = Math.max(currentXp, 0);
+        Level currentLevel = fromXp(safeXp);
+        Level nextLevel = currentLevel.getNextLevel();
+        
+        if (currentLevel == nextLevel) {
+            return 0; // 이미 최고 레벨
+        }
+        
+        return nextLevel.minXp - safeXp;
+    }
+
+    public Level getNextLevel() {
+        return switch (this) {
+            case MOURNER -> PATIENT;
+            case PATIENT -> CORPSE;
+            case CORPSE -> GHOST;
+            case GHOST -> DEMON;
+            case DEMON -> DEMON;
+        };
     }
 }
